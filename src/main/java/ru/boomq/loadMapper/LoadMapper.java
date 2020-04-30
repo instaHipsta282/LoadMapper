6f63a1870c90d67a616b52bea829fbe3d2d16334
instaHipsta282 <45939689+instaHipsta282@users.noreply.github.com>
2020-04-30 10:59:37 +0300

    Update LoadMapper.java

--- src/main/java/ru/boomq/loadMapper/LoadMapper.java
+++ src/main/java/ru/boomq/loadMapper/LoadMapper.java
@@ -43,12 +43,30 @@ public class LoadMapper {
             timeLine.merge(i, 0L, Long::sum);
         }
 
+        //тут у меня стояла задача равномерно разделить общее количество пользователей на количество секунд
+        //rumpUp`a, поэтому я вычисляю среднее количество тредов, которые выводятся на этапе rump up за 1 секунду
         double secForThread = (double) startupTimeSec / startThreadsCount;
         double threadPerSec = 1 / secForThread;
 
         //startup
         int startupStepNumber = 0;
         for (long i = initDelaySec; i < initDelaySec + startupTimeSec; i++) {
+            //Тут я умножаю секунду rump up`a(от 0 до конца rump up) на количество тредов в секунду и срезаю дробную
+            // часть 
+            // Допустим, у нас rumpUp 10 секунд, и надо вывести 25 пользователей: secForThread: 10 / 25 = 0.4
+            // threadPerSec: 1 / 0.4 = 2.5 thread в секунду.
+            // в первую секунду rump up выводится 2.5 == 2 thread,
+            // во вторую, 2.5 * 2 = 5
+            // в третью 2.5 * 3 = 7.5 == 7
+            // в четвертую 2.5 * 4 = 10
+            // в четвертую 2.5 * 5 = 12.5 == 12
+            // в четвертую 2.5 * 6 = 15
+            // в четвертую 2.5 * 7 = 17.5 == 17
+            // в четвертую 2.5 * 8 = 20
+            // в четвертую 2.5 * 9 = 22.5 == 22
+            // в четвертую 2.5 * 10 = 25
+            // в итоге мы сделали равномерное повышение нагрузки с 0 до 25
+            // rump down работает аналогично, но с понижением
             timeLine.merge(i, (long) (threadPerSec * startupStepNumber), Long::sum);
             startupStepNumber++;
         }
